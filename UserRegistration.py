34053eda0fbdff62b358ac6eb4e8dd91cbf1fc8b
unknown <mfjkd7795@gmail.com>
2021-09-27 08:01:18 -0700

    [Mohammad] Add: User Registartion program using Python

--- UserRegistration.py
+++ UserRegistration.py
@@ -1,9 +1,67 @@
 """
 * @Author: Mohammad Fatha
-* @Date: 2021-09-27 00:44
+* @Date: 2021-09-27 20:20
 * @Last Modified by: Mohammad Fatha
-* @Last Modified time: 2021-09-27 00:44 
+* @Last Modified time: 2021-09-27 20:20 
 * @Title: :Python program for User Registartion
 """
-print("Welcome to the User Registration program using Python")
+from Validation import Validation
+import unittest
 
+class UserValidationTest(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_First_Name_True(self):
+        """
+        Description: 
+            In this test case when given a valid first name should return true.
+        """        
+        self.assertTrue(Validation.validate_name("Mohammad"))
+        self.assertTrue(Validation.validate_name("Fat"))
+
+
+    def test_First_Name_False(self):
+        """
+        Description: 
+            In this test case when given a invalid first name should return false.
+        """        
+        self.assertFalse(Validation.validate_name("fatha"))
+        self.assertFalse(Validation.validate_name("F@tha"))
+    
+    def test_Last_Name_True(self):
+        """
+        Description: 
+            In this test case when given a valid last name should return true.
+        """        
+        self.assertTrue(Validation.validate_name("Jamkhandi"))
+        self.assertTrue(Validation.validate_name("Jam"))
+    
+    def test_Last_Name_False(self):
+        """
+        Description: 
+            In this test case when given a invalid last name should return false.
+        """        
+        self.assertFalse(Validation.validate_name("jamkhandi"))
+        self.assertFalse(Validation.validate_name("j@am"))
+    
+    def test_Email_ID_True(self):
+        """
+        Description: 
+            In this test case when given a email should return true.
+        """        
+        self.assertTrue(Validation.validate_email("mfjkd7795@gmail.com"))
+        self.assertTrue(Validation.validate_email("abc.xyz@bl.co.in"))
+    
+    def test_Email_ID_False(self):
+        """
+        Description: 
+            In this test case when given a email should return flase.
+        """        
+        self.assertFalse(Validation.validate_email("mfjkd7795gmail.com"))
+        self.assertFalse(Validation.validate_email("abc.xyzbl.co.in"))
+
+if __name__ == '__main__':
+    unittest.main()
+    

c3a739805d76e3ee6c83c399805e07c80b0d15fc
unknown <mfjkd7795@gmail.com>
2021-09-27 08:02:03 -0700

    [Mohammad] Add: Log handler code with info and error level

---
+++ LoggerHandler.py
@@ -0,0 +1,11 @@
+import logging
+
+logger = logging
+
+# Basic configuration and required format of the file
+
+logger.basicConfig(filename="F:\.metadata\Python_User_Registration\ UserRegistration.log",
+                    level=logging.INFO, format='%(asctime)s:%(funcName)s:%(levelname)s:%(message)s')
+
+logger.basicConfig(filename="F:\.metadata\Python_User_Registration\ UserRegistration.log",
+                    level=logging.ERROR, format='%(asctime)s:%(funcName)s:%(levelname)s:%(lineo)d')

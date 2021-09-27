7f3add7930791ce0327d441a67e871029fd925f1
unknown <mfjkd7795@gmail.com>
2021-09-27 08:01:48 -0700

    [Mohammad] Add: Program to validate the email ID using Regex

---
+++ Validation.py
@@ -0,0 +1,46 @@
+"""
+* @Author: Mohammad Fatha
+* @Date: 2021-09-27 19:44
+* @Last Modified by: Mohammad Fatha
+* @Last Modified time: 2021-09-27 19:44 
+* @Title: : Python program for User Registration
+"""
+
+import re
+from LoggerHandler import logger
+
+class Validation():
+
+    def validate_name(name):
+        """
+        Description:
+            This function is to validate first and last name.
+            Condition: Name should start with Capital letter and should have min 3 letters.
+        Args:
+            firstName: First and last name to validate
+        Returns:
+            boolean result
+        """        
+        try:
+            return bool(re.match("^[A-Z]{1}[a-z]{2,}$", name))
+        except Exception as e:
+            logger.error(e)
+    
+    def validate_email(email):
+        """
+        Description:
+            E.g. abc.xyz@bl.co.in - Email has 3 mandatory parts (abc, bl & co) and 2 optional
+             (xyz & in) with precise @ and . positions
+        Args:
+            firstName: First and last name to validate
+        Returns:
+            boolean result
+        """        
+        try:
+            return bool(re.match("^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$", email))
+        except Exception as e:
+            logger.error(e)
+    
+
+
+

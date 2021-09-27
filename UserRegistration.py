0d84d18f774e3f2e9d55c56b7f5bf5affc802cd5
unknown <mfjkd7795@gmail.com>
2021-09-27 06:00:50 -0700

    [Mohammad] Add: User Registartion program using Python

---
+++ UserRegistration.py
@@ -0,0 +1,9 @@
+"""
+* @Author: Mohammad Fatha
+* @Date: 2021-09-27 00:44
+* @Last Modified by: Mohammad Fatha
+* @Last Modified time: 2021-09-27 00:44 
+* @Title: :Python program for User Registartion
+"""
+print("Welcome to the User Registration program using Python")
+

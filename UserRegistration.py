8abbfb8698a7c031bf7a563aa120ec873102345e
unknown <mfjkd7795@gmail.com>
2021-09-27 06:42:18 -0700

    [Mohammad] Add: Unit Test case to validate the First name

--- UserRegistration.py
+++ UserRegistration.py
@@ -5,5 +5,28 @@
 * @Last Modified time: 2021-09-27 00:44 
 * @Title: :Python program for User Registartion
 """
-print("Welcome to the User Registration program using Python")
+from Validation import Validation
+import unittest
 
+class UserValidationTest(unittest.TestCase):
+
+    def test_ValidFirstName(self):
+        """
+        Description: 
+            In this test case when given a valid first name should return true.
+        """        
+        self.assertTrue(Validation.validate_name("Mohammad"))
+        self.assertTrue(Validation.validate_name("Fat"))
+
+
+    def test_InvalidFirstName(self):
+        """
+        Description: 
+            In this test case when given a invalid first name should return false.
+        """        
+        self.assertFalse(Validation.validate_name("fatha"))
+        self.assertFalse(Validation.validate_name("F@tha"))
+
+if __name__ == '__main__':
+    unittest.main()
+    
